49ee08777a761c4c87eb256cda2c2fdb0f96ca4a
bluff <bluffyx91@gmail.com>
2022-11-24 21:47:42 +0200

    created useFetch, RecipeList and tweaked the Home components

---
+++ src/components/RecipeList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './RecipeList.css'
+import { Link } from 'react-router-dom'
+
+function RecipeList(props) {
+    return (
+        <div className='recipe-list'>
+            { props.recipes.map(item => (
+                <div className='card' key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>{item.cookingTime} to make.</p>
+                    <div>{item.method.substring(0,100)}...</div>
+                    <Link to={`/recipes/${item.id}`}>Cook this</Link>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default RecipeList;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,15 @@
 import "./Home.css"
 import React from 'react';
+import { useFetch } from "../../hooks/useFetch";
+import RecipeList from "../../components/RecipeList";
 
 function Home(props) {
+    const {data, pending, error} = useFetch('http://localhost:3000/recipes')
     return (
-        <div>
-            Home
+        <div className="home">
+            {error && <p className="error">{error}</p>}
+            {pending && <p className="loading">Loading...</p>}
+            {data && <RecipeList recipes={data} />}
         </div>
     );
 }
