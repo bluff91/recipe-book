035f225714d793add87bf6b833ba49ff944dec6c
bluff <bluffyx91@gmail.com>
2022-11-29 22:57:07 +0200

    created the form component and added its logic and css

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,57 @@
 import "./Create.css"
-import React from 'react';
+import React, { useState } from 'react';
 
 function Create(props) {
+    const [title, setTitle] = useState("")
+    const [method, setMethod] = useState("")
+    const [time, setTime] = useState("")
+    
+
+    const addRecipe = (e) => {
+        e.preventDefault()
+        console.log(time,method,title)
+
+    }
+
+   
+
     return (
-        <div>
-           Create dDAdasda !!! 
+        <div className="create">
+            <h2 className="page-title">Add a new Recipe</h2>
+            <form onSubmit={addRecipe}>
+                <label htmlFor="title">Recipe Title:</label>
+                <input 
+                    type='text' 
+                    id='title' 
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                >
+                </input>
+
+                {/* // ingredients here */}
+
+                <label htmlFor="method">Cooking Method:</label>
+                <textarea  
+                    id='method' 
+                    value={method} 
+                    onChange={(e) => setMethod(e.target.value)}
+                    required
+                >
+                </textarea>
+
+                <label htmlFor="time">Cooking Time(minutes):</label>
+                <input 
+                    type='number' 
+                    id='time' 
+                    value={time} 
+                    onChange={(e) => setTime(e.target.value)}
+                    required
+                >
+                </input>
+
+                <button className="btn">Create Recipe</button>
+           </form> 
         </div>
     );
 }
